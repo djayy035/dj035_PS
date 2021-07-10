11a7789d25b7c6143035b5d8c75908af676c2d85
dj035 <77890454+djayy035@users.noreply.github.com>
2021-07-10 23:54:06 +0900

    Create A.cpp

---
+++ AtCoder/AtCoder Beginner Contest 209/A.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define MEM 56
+#define sanic ios_base::sync_with_stdio(0)
+#define x first
+#define y second
+using namespace std;
+typedef long long ll;
+typedef pair<ll, ll> pi;
+const ll INF = 2e18+9;
+const ll MOD = 1e9+7;
+ll t,n,m,k;
+ll a[MEM];
+int main(){
+    sanic;
+    cin >> n >> m;
+    cout << max(m-n+1,0LL);
+}
