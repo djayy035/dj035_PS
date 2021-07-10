51c8213b5fdb5511a7ca99c073e048195b4ac161
dj035 <77890454+djayy035@users.noreply.github.com>
2021-07-10 09:55:36 +0900

    Create A.cpp

---
+++ Codeforces/Codeforces Round #473 (Div. 2)/A.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define MEM 5005
+#define sanic ios_base::sync_with_stdio(0)
+#define x first
+#define y second
+using namespace std;
+typedef long long ll;
+typedef pair<ll, ll> pi;
+const ll INF = 2e18+9;
+const ll MOD = 1e9+7;
+ll t,n,m,k;
+ll a[MEM];
+int main(){
+    sanic;
+    cin >> n;
+    if(n%2) cout << "Ehab";
+    else cout << "Mahmoud";
+}
