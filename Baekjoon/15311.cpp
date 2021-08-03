719f0fedbef446425150011f661c5e6281f7de1a
dj035 <77890454+djayy035@users.noreply.github.com>
2021-08-04 00:48:37 +0900

    Create 15311.cpp

---
+++ Baekjoon/15311.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define MEM 500005
+#define sanic ios_base::sync_with_stdio(0)
+#define x first
+#define y second
+using namespace std;
+typedef long long ll;
+typedef pair<ll, ll> pi;
+const ll INF = 1e13;
+const ll MOD = 1234567891;
+ll t,n,m,k,c,ans;
+int main(){
+    sanic; cin.tie(0); cout.tie(0);
+    cin >> n;
+    cout << 2000 << '\n';
+    for(int i=0; i<1000; i++)
+        cout << 1000 << ' ';
+    for(int i=0; i<1000; i++)
+        cout << 1 << ' ';
+}
